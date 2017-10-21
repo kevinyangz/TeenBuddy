acb44360a187bc5c5bcc0f64458738899b7aab69
kevinyangz <kevinze.yang@mail.utoronto.ca>
2017-10-21 15:41:48 -0400

    Add column age into Teenager table
    A test

--- TeenBuddy/db/migrate/20171016210834_create_teenagers.rb
+++ TeenBuddy/db/migrate/20171016210834_create_teenagers.rb
@@ -2,10 +2,12 @@ class CreateTeenagers < ActiveRecord::Migration[5.1]
   def change
     create_table :teenagers do |t|
       t.string :name
+      t.text :email_address
       t.text :home_address
       t.string :home_phone
       t.string :cell_phone
       t.date :birth_date
+
 
       t.timestamps
     end
