9262396d1dc82af93faee3cb6027b108f4443646
Ian <shan.wuzhen@mail.utoronto.ca>
2017-10-22 19:20:51 -0400

    Set up user authentication

--- TeenBuddy/config/routes.rb
+++ TeenBuddy/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :posts
   resources :clients
   resources :teenagers
   resources :services
 
-  root 'posts#index'
+  root 'home#index'
   get  'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
