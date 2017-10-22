ddcbc5fdba0dd569bfe0cfd035b1b290d7b376d2
kevinyangz <32553169+kevinyangz@users.noreply.github.com>
2017-10-22 18:17:54 -0400

    Delete 20171022190308_add_teenager_tags.rb

--- TeenBuddy/db/migrate/20171022190308_add_teenager_tags.rb
+++
@@ -1,6 +0,0 @@
-class AddTeenagerTags < ActiveRecord::Migration[5.1]
-  def change
-  	    add_column :teenagers, :tags, :string, array:true ,default: [] 
-  	    add_column :teenagers, :ratings, :integer, array:true, default: []
-  end
-end
