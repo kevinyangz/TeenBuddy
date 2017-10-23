005afa7816bdb669f7c55fe1cbf5ff997b4e36d2
Mattlih <mattlhy77@gmail.com>
2017-10-22 20:37:24 -0400

    Add Client seeds

--- TeenBuddy/db/seeds.rb
+++ TeenBuddy/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+many_clients = [{fname:"Yun", lname: "Ma", home_address: "123 Bay street", home_phone: "6471234567", cell_phone: "6472234567"},
+                {fname:"Jun", lname: "Lei", home_address: "223 Bay street", home_phone: "6472234567", cell_phone: "6472234567"},
+                {fname:"Huateng", lname: "Ma", home_address: "323 Bay street", home_phone: "6473234567", cell_phone: "6472234567"},
+                {fname:"Jianlin", lname: "Wang", home_address: "423 Bay street", home_phone: "6474234567", cell_phone: "6472234567"},
+                {fname:"Jiacheng", lname: "Li", home_address: "523 Bay street", home_phone: "6475234567", cell_phone: "6472234567"}]
+many_clients.each do |c|
+  Client.create(c)
+end
