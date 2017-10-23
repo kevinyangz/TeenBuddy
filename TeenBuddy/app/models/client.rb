64b1c007b0807bb6fc15eab8924d3fc6c31d6b6b
Mattlih <haoyuan.li@mail.utoronto.ca>
2017-10-22 21:52:59 -0400

    Fixed a typo

--- TeenBuddy/app/models/client.rb
+++ TeenBuddy/app/models/client.rb
@@ -2,5 +2,5 @@ class Client < ApplicationRecord
   has_many :posts
 
   validates :fname, :lname, :homeaddress, :cell_phone, presence: true
-  validates :fname, :lname, length: { minimun: 2 }
+  validates :fname, :lname, length: { minimum: 2 }
 end
