1bce0888ecb50976e78c48adbceea6e174ac5558
Chengchen Yang <alphonse.yang@mail.utoronto.ca>
2017-10-29 17:20:20 -0400

    Remove migration file that causes migration failure

--- TeenBuddy/db/migrate/20171029183019_remove_service_type_from_post.rb
+++
@@ -1,8 +0,0 @@
-class RemoveServiceTypeFromPost < ActiveRecord::Migration[5.1]
-  def change
-  	remove_column :posts, :service_category
-  	remove_column :posts, :service_type
-  	remove_reference :posts, :ServiceType
-  	drop_table :service_types
-  end
-end
