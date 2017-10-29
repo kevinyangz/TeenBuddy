db3f5fc21c577c54a2ade56034534f99907a4610
liwuqi95 <wuqi.li@mail.utoronto.ca>
2017-10-29 18:51:20 -0400

    Setup css files and javascripts. Rewrite login page with new css setup

--- TeenBuddy/db/schema.rb
+++ TeenBuddy/db/schema.rb
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 20171029201248) do
     t.integer "number_of_teenager_needed"
     t.integer "post_status"
     t.integer "service_id"
-    t.string "service_title"
     t.integer "service_category_id"
     t.integer "service_type_id"
     t.index ["client_id"], name: "index_posts_on_client_id"
@@ -77,6 +76,7 @@ ActiveRecord::Schema.define(version: 20171029201248) do
   end
 
   create_table "teenagers", force: :cascade do |t|
+    t.text "email_address"
     t.text "home_address"
     t.string "cell_phone"
     t.date "birth_date"
@@ -84,7 +84,6 @@ ActiveRecord::Schema.define(version: 20171029201248) do
     t.datetime "updated_at", null: false
     t.string "fname"
     t.string "lname"
-    t.integer "age"
     t.string "postal_code"
     t.integer "user_id"
     t.index ["user_id"], name: "index_teenagers_on_user_id"
