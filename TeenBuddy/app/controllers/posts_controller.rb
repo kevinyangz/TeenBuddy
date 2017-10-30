38061ef1570b0555f19cc8b4f367ad3eecd68a95
liwuqi95 <wuqi.li@mail.utoronto.ca>
2017-10-30 15:49:27 -0400

    Fix bug that select options cannot be loaded on the first load.

--- TeenBuddy/app/controllers/posts_controller.rb
+++ TeenBuddy/app/controllers/posts_controller.rb
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to posts_path {client_id} }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
