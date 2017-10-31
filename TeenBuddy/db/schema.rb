82fd1aff2a530c9b057adc23c07f05987d67b57f
liwuqi95 <wuqi.li@mail.utoronto.ca>
2017-10-30 20:39:28 -0400

    remove model job_application

--- TeenBuddy/db/schema.rb
+++ TeenBuddy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171029201248) do
+ActiveRecord::Schema.define(version: 20171031003814) do
 
   create_table "clients", force: :cascade do |t|
     t.string "fname"
@@ -22,15 +22,6 @@ ActiveRecord::Schema.define(version: 20171029201248) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_clients_on_user_id"
-  end
-
-  create_table "job_applications", force: :cascade do |t|
-    t.integer "post_id"
-    t.integer "teenager_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_job_applications_on_post_id"
-    t.index ["teenager_id"], name: "index_job_applications_on_teenager_id"
   end
 
   create_table "posts", force: :cascade do |t|
