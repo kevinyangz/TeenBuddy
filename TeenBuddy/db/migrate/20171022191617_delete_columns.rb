1a9ba27433e664e6372cbd5c072db38e55d0c67b
kevinyangz <kevinze.yang@mail.utoronto.ca>
2017-11-02 19:26:38 -0400

    Re-edit initial database
    1. Add users and corresponding teenagers or clients
    2. Add "delete_all" before create new clients/teenager to avoid conflict in seeds.rb
    3. Add TeenagerInterest initialization
    4. Delete attribute: email_address from Teenager Table

--- TeenBuddy/db/migrate/20171022191617_delete_columns.rb
+++ TeenBuddy/db/migrate/20171022191617_delete_columns.rb
@@ -2,5 +2,6 @@ class DeleteColumns < ActiveRecord::Migration[5.1]
   def change
   	remove_column :teenagers, :home_phone, :string
     add_column :teenagers, :postal_code, :string
+    remove_column :teenagers, :email_address, :text
   end
 end
