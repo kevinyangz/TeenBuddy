6a562763ff9d560b3f0750789082b1c6d2f166c5
liwuqi95 <wuqi.li@mail.utoronto.ca>
2017-11-06 22:34:47 -0500

    dynamic showing post application status at post_show page. add helper function to select style

--- TeenBuddy/app/helpers/post_applications_helper.rb
+++ TeenBuddy/app/helpers/post_applications_helper.rb
@@ -1,2 +1,13 @@
 module PostApplicationsHelper
+
+
+  def app_inv_status_style(status)
+    if status == 'Approved'
+      'btn-success'
+    elsif status == 'Rejected'
+      'btn-danger'
+    else
+      'btn-warning'
+    end
+  end
 end
