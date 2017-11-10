3652b1729675b495e3e54d8d343bfdeb58a98e2b
liwuqi95 <wuqi.li@mail.utoronto.ca>
2017-11-10 13:49:13 -0500

    display on service teenager

--- TeenBuddy/app/controllers/post_applications_controller.rb
+++ TeenBuddy/app/controllers/post_applications_controller.rb
@@ -6,10 +6,8 @@ class PostApplicationsController < ApplicationController
   def index
     if current_user.client
       @post_applications = PostApplication.joins(:post).where(['posts.client_id= ?',current_user.client.id])
-      @state ='client_applications'
     elsif current_user.teenager
       @post_applications=PostApplication.where(teenager_id: current_user.teenager.id)
-      @state ='teenager_applications'
     end
   end
 
