fc42463f89814c607e7a12b2c45d67eba15e615b
wuzhensh <shan.wuzhen@mail.utoronto.ca>
2017-11-11 14:45:08 -0500

    change title and wording

--- TeenBuddy/app/models/post_invitation.rb
+++ TeenBuddy/app/models/post_invitation.rb
@@ -1,4 +1,12 @@
 class PostInvitation < ApplicationRecord
   belongs_to :post
   belongs_to :teenager
+  after_update :initialize_service
+
+  def initialize_service
+    if self.status == 'Approved'
+      new_service = Service.new(teenager_id: self.teenager_id, post_id: self.post_id, client_id: self.post.client_id)
+      new_service.save
+    end
+  end
 end
