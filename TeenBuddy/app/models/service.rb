eacad0630a6aa9f0790fd217b0fc2e0782556924
liwuqi95 <wuqi.li@mail.utoronto.ca>
2017-11-11 13:11:59 -0500

    remove puts message in service model

--- TeenBuddy/app/models/service.rb
+++ TeenBuddy/app/models/service.rb
@@ -8,9 +8,6 @@ class Service < ApplicationRecord
 
   def set_post_status
     my_post = Post.find(self.post_id)
-    puts my_post.services.count
-    puts my_post.number_of_teenager_needed
-
     if my_post.services.count >= my_post.number_of_teenager_needed
       my_post.update(status:'close')
     else
