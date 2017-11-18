0877dae9798ac225bcd661337a65fd7f060292ac
wuzhensh <shan.wuzhen@mail.utoronto.ca>
2017-11-17 21:34:37 -0500

    Add corner case for matching system, one side ask the other side when the other side already requested.

--- TeenBuddy/app/models/post_invitation.rb
+++ TeenBuddy/app/models/post_invitation.rb
@@ -1,10 +1,10 @@
 class PostInvitation < ApplicationRecord
   belongs_to :post
   belongs_to :teenager
-  after_update :initialize_service
+  after_save :initialize_service
 
   def initialize_service
-    if self.status == 'Approved'
+    if self.status == 'Approved' || PostApplication.where(teenager_id: self.teenager_id, post_id: self.post_id)
       new_service = Service.new(teenager_id: self.teenager_id, post_id: self.post_id, client_id: self.post.client_id)
       new_service.save
     end
