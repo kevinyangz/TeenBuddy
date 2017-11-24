f2a4e97177711c43fd5b30c7c94c10805284bcac
liwuqi95 <wuqi.li@mail.utoronto.ca>
2017-11-23 19:30:20 -0500

    re write client form.

--- TeenBuddy/app/models/client.rb
+++ TeenBuddy/app/models/client.rb
@@ -3,7 +3,7 @@ class Client < ApplicationRecord
   has_many :services
 
   belongs_to :user
-  validates :fname, :lname, :home_address, :cell_phone, presence: true
+  validates :fname, :lname, presence: true
   validates :fname, :lname, length: { minimum: 2 }
 
     mount_uploader :selfie, SelifieUploader
