71452fbb50df7325f399cdda52dc1483d62548fd
bobxy <bobxiao.yang@mail.utoronto.ca>
2017-11-26 11:42:55 -0500

    update about us page

--- TeenBuddy/db/schema.rb
+++ TeenBuddy/db/schema.rb
@@ -174,6 +174,7 @@ ActiveRecord::Schema.define(version: 20171120155117) do
   end
 
   create_table "teenagers", force: :cascade do |t|
+    t.text "email_address"
     t.text "home_address"
     t.string "cell_phone"
     t.date "birth_date"
