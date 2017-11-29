3a9c69fcc165747bfd302a3f694f87f6fb770d02
liwuqi95 <wuqi.li@mail.utoronto.ca>
2017-11-29 16:05:17 -0500

    rewrite facebook login. stored useful information.

--- TeenBuddy/app/models/user.rb
+++ TeenBuddy/app/models/user.rb
@@ -17,13 +17,19 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first
+     where(provider: auth.provider, uid: auth.uid).first
+
   end
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
+      if data = session["devise.facebook_data"]
+        user.email = data["info"]["email"] if user.email.blank?
+        user.provider = data["provider"]
+        user.uid = data["uid"]
+        user.password = Devise.friendly_token[0,20]
+        puts 'here is the provider'
+        puts( session["devise.facebook_data"].map{ |k,v| "#{k} => #{v}" }.sort )
       end
     end
   end
