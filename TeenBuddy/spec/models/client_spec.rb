46fdd507daae027b4f9dc30e9a0d4efd1a0cefa4
Mattlih <haoyuan.li@mail.utoronto.ca>
2017-12-01 15:15:35 -0500

    update rspec

--- TeenBuddy/spec/models/client_spec.rb
+++ TeenBuddy/spec/models/client_spec.rb
@@ -3,9 +3,12 @@ require 'rails_helper'
 RSpec.describe Client, type: :model do
 
   it "is valid with valid attributes" do
-    #c = Client.new(fname: "matt" , lname: "li", home_address: "123 bay" )
+    @c = Client.new(fname: "matt" , lname: "li", home_address: "123 bay", description: "I'm not fat", home_phone: "123456789", cell_phone:"123456789" )
 
-   # expect(c).to be_valid
+    @c.available_credit = 0
+    @c.save
+
+    expect(@c).to be_valid
   end
 
   it "is not valid without a name" do
@@ -13,7 +16,9 @@ RSpec.describe Client, type: :model do
 
     expect(c).to_not be_valid
   end
+
   it "is not valid without a description"
+
   it "is not valid without an address"
   it "is not valid without a phone number"
 end
