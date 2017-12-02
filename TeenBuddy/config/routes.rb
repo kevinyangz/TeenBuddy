0162dde2437a10314205185e420ac3808dcc4d84
kevinyangz <kevinze.yang@mail.utoronto.ca>
2017-12-01 21:34:00 -0500

    Add authentication support for Admin Page
    1. Add Admin Role

--- TeenBuddy/config/routes.rb
+++ TeenBuddy/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :messages, only: [:new, :create]
   resources :conversations, only: [:index, :show, :destroy] do
