4608823f6d885df02973b8ebc35addaaf5b46bde
bobxy <bobxiao.yang@mail.utoronto.ca>
2017-12-03 16:56:53 -0500

    add production mode email verification

--- TeenBuddy/config/environments/production.rb
+++ TeenBuddy/config/environments/production.rb
@@ -96,5 +96,13 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :address              =>  'smtp.sendgrid.net',
+    :port                 =>  '587',
+    :authentication       =>  :plain,
+    :user_name            =>  'app80101197@heroku.com',
+    :password             =>  'y30nttsp3798',
+    :domain               =>  'heroku.com',
+    :enable_starttls_auto  =>  true
+  }
 end
