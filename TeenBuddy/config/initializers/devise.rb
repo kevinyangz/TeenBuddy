3f8ca7eefaecd551b62ba9c2d610760ff22343b4
bobxy <bobxiao.yang@mail.utoronto.ca>
2017-12-03 17:46:48 -0500

    fixed a typo

--- TeenBuddy/config/initializers/devise.rb
+++ TeenBuddy/config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # with default "from" parameter.
 
   if Rails.env.production?
-    config.mailer_sender = "teenbuddy.dontreply@gmail.com"
+    config.mailer_sender = "teenbuddy.donotreply@gmail.com"
   elsif Rails.env.development?
     config.mailer_sender = "teenbuddy.donotreply@gmail.com"
   end
