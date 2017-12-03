75c506e3097847856653b83a62172c1838d8143e
Chengchen Yang <alphonse.yang@mail.utoronto.ca>
2017-12-03 17:21:42 -0500

    calculate total profit from users

--- TeenBuddy/app/models/transaction.rb
+++ TeenBuddy/app/models/transaction.rb
@@ -4,4 +4,9 @@ class Transaction < ApplicationRecord
   # validation
   validates :amount, numericality:{greater_than:0}
 
+  def self.getTotalProfit
+  	totalAmount = Transaction.all.where('comment LIKE ?', "Deposit (We charge 5% service fee.)").sum(:amount)
+  	totalAmount = (totalAmount/0.95)*0.05/100
+  end
+
 end
