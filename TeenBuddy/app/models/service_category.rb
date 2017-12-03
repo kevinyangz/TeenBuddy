ebb8e6d11ca02d8f7f5d68f58e6a7236d9726a86
liwuqi95 <wuqi.li@mail.utoronto.ca>
2017-12-02 20:18:46 -0500

    add icons for service_category

--- TeenBuddy/app/models/service_category.rb
+++ TeenBuddy/app/models/service_category.rb
@@ -5,5 +5,22 @@ class ServiceCategory < ApplicationRecord
 
 		#add a join table relationship for teenag
 	has_many :teenager_interests, :dependent => :delete_all
-	has_many :teenagers, through: :teenager_interests 
+	has_many :teenagers, through: :teenager_interests
+
+
+	def get_fa
+		case self.title
+
+			when"Babysitting" then'fa-child'
+			when  "Yard Work" then 'fa-leaf'
+			when "Snow Shoveling" then 'fa-snowflake-o'
+			when "Furniture Moving"then
+			when "Simple Cleaning Tasks"then
+			when "Vacation Services"then
+			when "Dog Walking"then
+			when "Computer Help"then
+			when "Tutoring" then
+			when "Reading"then
+		end
+	end
 end
