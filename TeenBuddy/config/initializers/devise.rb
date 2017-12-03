12b798aad1488e1ed04bc2c7ab4880596b47bdfc
liwuqi95 <wuqi.li@mail.utoronto.ca>
2017-12-03 17:18:02 -0500

    fix email confirmation localhost

--- TeenBuddy/config/initializers/devise.rb
+++ TeenBuddy/config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'shanwuzhen@hotmail.com'
+  config.mailer_sender = 'teenbuddy.donotreply@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
