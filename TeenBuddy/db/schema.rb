892b7992df9d24bbe1fea7468c924655c7f35de4
liwuqi95 <wuqi.li@mail.utoronto.ca>
2017-12-04 00:03:49 -0500

    modify profile page picture.

--- TeenBuddy/db/schema.rb
+++ TeenBuddy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171204044753) do
+ActiveRecord::Schema.define(version: 20171203200458) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
